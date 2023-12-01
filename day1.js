55f2084de85d63943fc5be6a994bac808940b99b
Brian Bud <hello@brianbud.com>
2023-12-01 12:13:07 -0500

    split str to array and loop through each char

---
+++ day1.js
@@ -0,0 +1,22 @@
+// On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+
+// For example:
+
+// 1abc2
+// pqr3stu8vwx
+// a1b2c3d4e5f
+// treb7uchet
+
+// In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
+
+let data = `five8b
+2733vmmpknvgr
+3oneeighttwo`;
+
+const myArr = data.split("\n");
+
+myArr.forEach((str) => {
+  Array.from(str).forEach((char) => {
+    console.log(char);
+  });
+});
